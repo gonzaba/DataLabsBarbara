5e83c35e43d50dc9e75700a9af3b4f687a6a21c0
barbara <barbara.gonzalez1@upr.edu>
2018-02-26 15:40:27 -0400

    test

---
+++ eclipse-workspace/ArrayListLab/src/arraylabex.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class arraylabex {
+
+	int maxRequest = 0;
+	int requestDone = 0;
+	
+	ArrayList<Integer> list = new ArrayList<>();
+	
+	
+	public void main(String[] args) {
+		
+		System.out.println("Please ");
+		
+	}
+	
+
+	
+	
+	
+}
